2ce2ae76757fba2537b5d2f124c373f1ea5019a9
Hatsune Miku <54656100+Yuki-Angel-99@users.noreply.github.com>
2020-01-15 18:34:45 +0800

    Add files via upload

--- azurlane/js.js
+++ azurlane/js.js
@@ -1,23 +1,18 @@
-(function(){
-var **theVid** = document.getElementById("theVideo");
-[].forEach.call(document.getElementsByClassName('btn'), function(btn) {
-btn.addEventListener('click', function(e) {
+(
+function myfunction(){
+var x = document.getElementById("myDIV");
 
-switch (e.target.id){
-case "btn1":
-    <iframe 
-	width="560" height="315" src="https://www.youtube.com/embed/lcAZiO9vqCQ" 
-	frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 
-	picture-in-picture" allowfullscreen>
-	</iframe>;
+switch (x.innerHTML){
+case 1:
+x.innerHTML="Azur Lane English Subtitle Episode 1 HD";
 break;
-case "btn2":
+case 2:
 theVid.src = "mp4/Captain_Video02.mp4";	
 break;
-case "btn3":
+case 3:
 console.log("button three selected!");
 break;
-case "btn4":
+case 4:
 console.log("button four selected!");
 break;
 default:
